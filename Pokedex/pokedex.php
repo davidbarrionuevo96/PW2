affdb2bbaec57577ba3347450e598ca955384049
DavidBarrionuevo <david.barrionuevo96@hotmail.com>
2018-09-23 21:48:08 -0300

    Primer Intento

--- Pokedex/pokedex.php
+++ Pokedex/pokedex.php
@@ -8,70 +8,80 @@
 <main class="main">
 <?php
 
-$pokemons=Array("Charmander"=>array('ataque'=>'Blaze',
-				'genero' => "<img class='genero' src='https://png.icons8.com/color/260/male.png'>",
-				'imagen' => "<img class='imagenes' src='https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png'>",
-				'tipoimg' => "<img class='imagtipo' src='https://pokeguide.neocities.org/Pic/fireicon.png'>"),
+		$conn = mysqli_connect("localhost","root","","Pokedatos");
+		
+		$c=0;
+		if (isset($_POST["whoisthat"])) 
+		{
+			$buscado= $_POST["whoisthat"];
+			$buscado= strtolower($buscado);
+			$buscado= ucfirst($buscado);
+			$c=0;
 
-				"Bulbasaur"=>array('genero'=>'Female','ataque'=>'Overgrow',
-				'genero' => "<img class='genero' src='https://png.icons8.com/color/260/female.png'>",
-				'imagen' => "<img class='imagenes' src='https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png'>",
-				'tipoimg' => "<img class='imagtipo' src='https://pokeguide.neocities.org/Pic/grassicon.png'>"),
+			$sql = 'select * from Pokemon';
+			$result=mysqli_query($conn, $sql);
+			while($asd=mysqli_fetch_assoc($result)){
 
-				"Squirtle"=>array('ataque'=>'Torrent',
-				'imagen' => "<img class='imagenes' src='https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png'>",
-				'genero' => "<img class='genero' src='https://png.icons8.com/color/260/male.png'>",
-				'tipoimg' => "<img class='imagtipo' src='https://pokeguide.neocities.org/Pic/watericon.png'>"));
-
-				if (isset($_POST["whoisthat"])) 
+				if ($asd["Descripcion"] == $buscado) 
 				{
-					$buscado= $_POST["whoisthat"];
-					$buscado= strtolower($buscado);
-					$buscado= ucfirst($buscado);
-					$c=0;
-					foreach ($pokemons as $nombrecampo1=>$contenido1)  {
-					if ($nombrecampo1==$buscado)
-						{
-						  echo 
-						  "<h2>".$buscado."</h2>".
 
-								  "<div>".$pokemons[$buscado]["imagen"]."</div>".
-							  
+					$sql = "select Pok.Descripcion Pokemon, Pok.Ataque, Pok.Imagen,G.Descripcion Genero,T.Descripcion Tipo,T.Imagen Imgtipo, G.Imagen Imggenero  
+					from Pokemon Pok join Genero G on G.id=Pok.Id_Genero 
+					Join Tipo T on T.Id=PT.Id_Tipo
+					Join Poke_Tipo PT on PT.Id_Pokemon=Pok.Id
+					Where Pok.Descripcion='".$buscado."'";
 
-								  "<div class='texto-contendor'>".$pokemons[$buscado]["tipoimg"]."</div>".
+					$result=mysqli_query($conn, $sql);
+
+					$rows=mysqli_fetch_assoc($result);
+						
+					echo 
+					"<h2>".$rows['Pokemon']."</h2>".
+
+					"<div class='texto-contendor '>"."<img class='imagenes' src=".$rows['Imagen'].">"."</div>".
+
+					"<div class='texto-contendor '>"."<img class='imagtipo' src=".$rows['Imgtipo'].">"."</div>".
+
+					"<div class='texto-contendor '>"."<img class='genero' src=".$rows['Imggenero'].">"."</div>".
+					
+
+					"<div class='texto-contendor attack'>".$rows['Ataque']."</div>";
+					
+
+					$c++;
+				}} 
 				
-							
-								  "<div class='texto-contendor '>".$pokemons[$buscado]["genero"]."</div>".
+				if ($c==0) 
+				{
+					$sql = 'select * from Pokemon';
+
+					$result=mysqli_query($conn, $sql);
+
+					while($rows=mysqli_fetch_assoc($result))
+					{
+
+						$imagen=$rows['Imagen'];
+
+						echo
+						"<div class='contenedor'>".
+						"<h2'>".$rows['Descripcion']."</h2>".
+						"<div class=''>"."<img class='imagenes' src=".$imagen.">"."</div>".
+						"</div>";
+					}	
+					$c++;	  		
+				}
 				
-							
-								  "<div class='texto-contendor attack'>".$pokemons[$buscado]["ataque"]."</div>";		
-							  $c++;
-							
-						  }
-						  }
-					  if ($c==0) 
-					  {
-						  all ($pokemons);
-					  }
-				} 
-				
-				
-				
-				Function all ($pokemons){
-					foreach ($pokemons as $nombrecampo2 => $contenido2) 
-							  {	
-								
-								echo
-								"<div class='contenedor'>".
-								"<h2>".$nombrecampo2."</h2>".
-								"<div class=''>".$contenido2["imagen"]."</div>".
-								  "</div>";
-							}}
-				?>
-<form method="post" action="pokedex.php" >
+			}
+
+			?>
+<!-- <form method="post" action="pokedex.php" >
 
 <input class="enviar" type="text" name="whoisthat">
-<button class="enviar" type="submit">Consulta</button>
+<button class="enviar" type="submit">Consulta</button> -->
+
+<form method="POST" action="pokedex.php" >
+				<input class="enviar" type="text" name="whoisthat" autofocus>
+			</form>
 </main>
 </form>
 </body>
